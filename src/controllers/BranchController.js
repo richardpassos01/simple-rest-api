6552889ff7f203af4b8447628bdb7115fa23a933
richardpassos01 <richard.passos@hotmail.com>
2020-08-17 21:53:47 -0300

    refactor(brances): make a branch controller beauty

--- src/controllers/BranchController.js
+++ src/controllers/BranchController.js
@@ -1,11 +1,43 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+let database;
+let Restaurant;
+
 class BranchController {
+    constructor() {
+        const mongo = mongoose.connect(process.env.MONGO_SRV, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        this.database = mongo;
+    }
+
     async getBranches({
         query: {
             id
         }
     }) {
-        console.log(id)
-        return 'branches';
+        database = database || await this.database
+
+        const restaurantShcema = new database.Schema({
+            name: String,
+            lastName: String,
+        });
+
+        Restaurant = Restaurant || database.model('Restaurant', restaurantShcema);
+
+        const queryFilter = id ? {
+            _id: id
+        } : {};
+
+        const item = await Restaurant.find(queryFilter, {
+            name: 1,
+            _id: 0
+        });
+
+        return item;
     }
 }
 
